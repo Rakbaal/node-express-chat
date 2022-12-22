d70d91de48572e39aa9bb1fa4a89a89967addd51
Leo Carpentier <leo.carpentier@overspeed.fr>
2022-12-22 08:31:13 +0100

    Oups, j'ai oublié de push hier

--- back-end/socket/chat.js
+++ back-end/socket/chat.js
@@ -1,17 +1,26 @@
 module.exports = function (io) {
+  let connectedUsers = []
+  let messages = []
 
   io.on('connection', (socket) => {
     console.log(`Connecté au client ${socket.id}`)
+    connectedUsers.push(socket.id)
+    console.log(connectedUsers);
     io.emit('notification', { type: 'new_user', data: socket.id });
+    io.emit('history', messages)
 
     // Listener sur la déconnexion
     socket.on('disconnect', () => {
       console.log(`user ${socket.id} disconnected`);
+      connectedUsers = connectedUsers.filter(x => x != socket.id)
       io.emit('notification', { type: 'removed_user', data: socket.id });
+      console.log(connectedUsers);
     });
 
-    socket.on('...', (msg) => {
-
+    socket.on('pushMessage', (msg) => {
+      socket.emit('notification', msg)
+      socket.emit('message', msg)
+      messages.push(msg)
     });
   })
 }
--- front-end/main.js
+++ front-end/main.js
@@ -1,14 +1,62 @@
+const server = 'http://127.0.0.1:3000'
+const socket = io(server);
+
+let me = null
+let userName = null
+
+const initHistory = (history) => {
+    console.log("TRIGGERED");
+    for (i = 0; i != history.length; i++) {
+        addToFeed(history[i])
+    }
+}
+
+const addToFeed = (message) => {
+    const authorDiv = document.createElement('div')
+        authorDiv.innerText = message.from
+        authorDiv.classList.add('name') 
+
+        const contentDiv = document.createElement('div')
+        contentDiv.innerText = message.content
+        contentDiv.classList.add('message')
+
+        const liElement = document.createElement('li')
+        message.socketId === me ? liElement.classList.add("me") : ""
+        liElement.appendChild(authorDiv)
+        liElement.appendChild(contentDiv)
+
+        document.getElementById('msgList').appendChild(liElement)
+}
+
 (function () {
-    const server = 'http://127.0.0.1:3000'
-    const socket = io(server);
+    socket.on('connect', () => {
+        me = socket.id
+        userName = prompt('UserName :')
+        document.getElementById('userName').innerText = userName
+        socket.emit('chosenUserName', userName)
+    })
+
 
     socket.on('notification', (data) => {
         console.log('Message depuis le seveur:', data);
     })
 
-    fetch(`${server}/test`).then((res) => {
-        return res.json()
-    }).then((data) => {
-        console.log(data);
+    socket.on('history', (history) => {
+        initHistory(history)
     })
 })()
+
+const inputMsg = document.getElementById('msg')
+const submitButton = document.getElementById('submit')
+
+submitButton.addEventListener('click', () => {
+    if(inputMsg.value != "") {
+        const msg = {
+            from: userName,
+            socketId: socket.id,
+            content: inputMsg.value
+        }
+        socket.emit('pushMessage', { msg })
+        inputMsg.value = ""
+    }
+})
